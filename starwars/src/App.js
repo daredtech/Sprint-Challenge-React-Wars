6750abc5a677b08608252539d73a85a0e53ed4bc
Red <vhanchar@ucsc.edu>
2019-07-19 10:51:37 -0700

    added charactercard component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import CharacterCard from './CharacterCard'
 
 import './App.css';
 
@@ -14,14 +15,16 @@ const App = () => {
 
   useEffect(() => {
     // to get a list of Star Wars characters 
-    axios.get('https://swapi.co/api/people/')
+    // axios.get('https://swapi.co/api/people/')
+    axios.get('https://henry-mock-swapi.herokuapp.com/api')
+    
     // to log the results if completed 
     .then(success => {
       console.log('request completed');
       console.log('data', success.data);
       console.log('results', success.data.results);
       // to set data to the state
-      setCharactersCollection(success.data.results);
+      setCharactersCollection(success.data.results[0]);
     })
     // to log the message if not completed 
     .catch(failure => {
@@ -31,11 +34,36 @@ const App = () => {
     ,[]
   )
 
+  console.log('first object: ', charactersCollection);
+  console.log('first object name: ', charactersCollection['name']);
+  const name=charactersCollection['name'];
+  const hair_color=charactersCollection['hair_color'];
+  let homeworld=charactersCollection['homeworld'];
+
+  if (homeworld=== "https://swapi.co/api/planets/1/") {homeworld='Tatooine'}
+  console.log('homeworld: ', homeworld);
+
+    // console.log('printing characters collection: ', charactersCollection);
+    // console.log('first object: ', charactersCollection[0]);
+    // let test = charactersCollection[0];
+    // if(test!== undefined){
+    //   console.log('name: ', test['number']);
+    // }
+    
+    // console.log('printing characters collection: ', charactersCollection);
+    // console.log('printing characters collection: ', charactersCollection);
+    // console.log('printing characters collection: ', charactersCollection);
+    // console.log('printing characters collection: ', charactersCollection);
+    // console.log('printing characters collection: ', charactersCollection);
+
+    // charactersCollection.forEach()
+
  
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <CharacterCard name={name} hair_color={hair_color} homeworld={homeworld}/>
     </div>
   );
 }
---
+++ starwars/src/CharacterCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Card, Feed} from 'semantic-ui-react'
+
+const CharacterCard = (props) => (
+    <Card>
+    <Card.Content>
+      <Card.Header>{props.name}</Card.Header>
+    </Card.Content>
+    <Card.Content>
+      <Feed>
+        <Feed.Event>
+          <Feed.Content>
+            haircolor: {props.hair_color}
+          </Feed.Content>
+          <Feed.Content>
+            homeworld: {props.homeworld}
+          </Feed.Content>
+        </Feed.Event>
+      </Feed>
+    </Card.Content>
+  </Card>
+ )
+
+    export default CharacterCard
