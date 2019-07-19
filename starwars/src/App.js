cecb22f24fb869208f9c413c93e8c6542d3da6df
Red <vhanchar@ucsc.edu>
2019-07-19 09:35:31 -0700

    set the data to the the state

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -10,18 +10,20 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [charactersCollection, setCharactersCollection] = useState('');
 
   useEffect(() => {
+    // to get a list of Star Wars characters 
     axios.get('https://swapi.co/api/people/')
-
+    // to log the results if completed 
     .then(success => {
       console.log('request completed');
       console.log('data', success.data);
       console.log('results', success.data.results);
-
-
+      // to set data to the state
+      setCharactersCollection(success.data.results);
     })
-
+    // to log the message if not completed 
     .catch(failure => {
       console.log('unable to complete the request');
     })
