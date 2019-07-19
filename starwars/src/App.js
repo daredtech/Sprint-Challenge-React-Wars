e6a0d7e7bcff55ba855b4ba27476b1c8320e59f7
Red <vhanchar@ucsc.edu>
2019-07-19 09:26:24 -0700

    got a list of people from swapi api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
 import './App.css';
 
 const App = () => {
@@ -8,6 +10,26 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people/')
+
+    .then(success => {
+      console.log('request completed');
+      console.log('data', success.data);
+      console.log('results', success.data.results);
+
+
+    })
+
+    .catch(failure => {
+      console.log('unable to complete the request');
+    })
+  }
+    ,[]
+  )
+
+ 
 
   return (
     <div className="App">
